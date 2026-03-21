52b6b926f181c78d1f622619f9716c4d7c3bfff0
Johnatan López <jhonatan12316@hotmail.com>
2026-03-21 16:26:45 -0500

    Feat: Se crea clase HabitacionVIP y se implementa el método heredado de la clase princial. Se realiza la validación de que si es mayor a 5 días, se cobrara el 0.80 (80%) del total calculado a partir de la duración de la estadía y la tarifa por noche.

---
+++ SistemaGestionReservas/Models/HabitacionVIP.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGestionReservas.Logic
+{
+    public class HabitacionVIP : Reserva
+    {
+
+        public override double CalcularCostoTotal()
+        {
+            double total = DuracionEstadia * TarifaNoche;
+
+            //Descuento del 20% si son más de 5 noches (Regla 4)
+            if (DuracionEstadia > 5)
+            {
+                total *= 0.80;
+            }
+            return total;
+        }
+
+    }
+}
