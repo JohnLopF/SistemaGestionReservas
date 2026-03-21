e8aea83b8559670b52b5e5d1f63ac7df21408806
Johnatan López <jhonatan12316@hotmail.com>
2026-03-21 16:49:39 -0500

    Feat: Se crea un método "AgregarReserva" que validará que la fecha de inicio y fin de la estadía con el fin de revisar si la misma habitación ya está ocupada en el mismo periodo de tiempo.

--- SistemaGestionReservas/Logic/AdministradorHotel.cs
+++ SistemaGestionReservas/Logic/AdministradorHotel.cs
@@ -11,8 +11,24 @@ namespace SistemaGestionReservas.Logic
 
         private List<Reserva> reservas;
 
-        
-        
+        public void AgregarReserva(Reserva nueva)
+        {
+            DateTime inicioNueva = nueva.FechaReserva.Date;
+            DateTime finNueva = inicioNueva.AddDays(nueva.DuracionEstadia);
+
+            bool ocupada = reservas.Any(r =>
+                r.NumeroHabitacion == nueva.NumeroHabitacion &&
+                inicioNueva < r.FechaReserva.AddDays(r.DuracionEstadia).Date &&
+                finNueva > r.FechaReserva.Date
+            );
+
+            if (ocupada)
+            {
+                throw new Exception($"La habitación {nueva.NumeroHabitacion} ya está ocupada " + $"entre las fechas seleccionadas.");
+            }
+
+        }
+
 
 
 
