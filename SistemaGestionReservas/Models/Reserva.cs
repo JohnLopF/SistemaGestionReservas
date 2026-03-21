214de2dc16e373e5edfe348dd5694322f1722c21
Johnatan López <jhonatan12316@hotmail.com>
2026-03-21 16:18:23 -0500

    Fix: Se realiza el cambio de carpeta de "Logic" a "Models" ya que la carpeta "Logic" estará destinada al manejo de las listas.

--- SistemaGestionReservas/Models/Reserva.cs
+++ SistemaGestionReservas/Models/Reserva.cs
@@ -22,6 +22,18 @@ namespace SistemaGestionReservas.Logic
         {
             return $"Reserva para {NombreCliente} (Documento: {DocumentoCliente}), Habitación: {NumeroHabitacion}, Fecha: {FechaReserva.ToShortDateString()}, Duración: {DuracionEstadia} noches, Tarifa por noche: {TarifaNoche:C}";
         }
-        
+
+        public virtual void Validar()
+        {
+            if (string.IsNullOrEmpty(NombreCliente) || NumeroHabitacion <= 0)
+                throw new Exception("El nombre y el número de habitación son obligatorios.");
+
+            if (DuracionEstadia <= 1)
+                throw new Exception("La reserva debe ser mayor a 1 noche (Regla 2).");
+
+            if (TarifaNoche <= 0)
+                throw new Exception("La tarifa debe ser mayor a cero (Regla 3).");
+        }
+
     }
 }
