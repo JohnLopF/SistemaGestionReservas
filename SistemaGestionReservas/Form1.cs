6da468291fbdd8a8c237502c2abeff6f52c336a7
Johnatan López <jhonatan12316@hotmail.com>
2026-03-21 19:27:26 -0500

    Feat: Se añade variable documentoEdicion para controlar si el dato que se ingresa es nuevo o una edición.

--- SistemaGestionReservas/Form1.cs
+++ SistemaGestionReservas/Form1.cs
@@ -14,6 +14,7 @@ namespace SistemaGestionReservas
     public partial class Form1 : Form
     {
         private AdministradorHotel admin = new AdministradorHotel();
+        private string documentoEdicion = "";
         public Form1()
         {
             InitializeComponent();
